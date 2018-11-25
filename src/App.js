630ba880d34c747ccfaffeb13da8d9ac8137fba1
Chie Suzuki <sakura.c.smile@gmail.com>
2018-11-25 22:22:46 +0900

    create class component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
- render() {
-   //const greeting = "Hi, Tom!";
-   //const dom = <h1 className="foo">{greeting}</h1>;
-   //return dom;
-   //return <input type="text" onClick={() => {console.log("I am clicked.")}} />;
-  return (
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onChange={() => {console.log("I am changed.")}} />
-    </React.Fragment>
-  )
-   //return <input type="text" onChange={() => {console.log("I am changed.")}} />;
-  }
+//クラスコンポーネント
+//class App extends Component {
+// render() {
+//  return (
+//    <React.Fragment>
+//      <label htmlFor="bar">bar</label>
+//      <input type="text" onChange={() => {console.log("I am changed.")}} />
+//    </React.Fragment>
+//  )
+//  }
+//}
+
+//関数コンポーネント
+const App = () => {
+  return <div>
+    <Cat />
+    <Cat />
+    <Cat />
+    <Cat />
+  </div>
 }
 
+const Cat = () => {
+  return <div>Meow!</div>
+}
 export default App;
